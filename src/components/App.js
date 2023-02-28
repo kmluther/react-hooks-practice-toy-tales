54d61c2d4576136bcd2d38b66b52f5497743e226
Katelyn Luther <katelynluther87@aim.com>
2023-02-28 11:11:36 -0700

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
+const toysAPI = "http://localhost:3001/toys";
+
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch(toysAPI)
+    .then(res => res.json())
+    .then(setToys);
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function addNewToy(name, image) {
+    const newToy = {
+      name,
+      image,
+      likes: 0,
+    };
+
+    console.log(name, image);
+    
+    fetch(toysAPI, {
+      method: 'POST',
+      headers: {
+        Accepts: 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(newToy),
+    })
+    .then(res => res.json())
+    .then(toy => setToys([...toys, toy]));
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm handleSubmit={addNewToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys}/>
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm(handleSubmit) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+
+  function submitForm(e) {
+    e.preventDefault();
+    handleSubmit(name, image)
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form onSubmit={submitForm} className="add-toy-form">
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
         <br />
         <input
@@ -17,6 +27,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={image}
+          onChange={e => setImage(e.target.value)}
         />
         <br />
         <input
